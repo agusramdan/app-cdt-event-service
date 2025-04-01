ba23a4ad81614f5cf984d8a5c7613a398c61fa47
Agus Muhammad Ramdan <agus.ramdan@gmail.com>
2025-04-01 20:39:56 +0700

    add @EnableKafka

--- src/main/java/agus/ramdan/cdt/event/MainApplication.java
+++ src/main/java/agus/ramdan/cdt/event/MainApplication.java
@@ -2,7 +2,9 @@ package agus.ramdan.cdt.event;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
+@EnableKafka
 @SpringBootApplication
 public class MainApplication {
 
